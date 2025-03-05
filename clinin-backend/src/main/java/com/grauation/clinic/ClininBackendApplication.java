360c6c54ada0a727b4dc403e2fd4920e3d8453dd
Mostafa Elwy <mostafaelwy154@gmail.com>
2025-03-05 02:29:27 +0200

    init

---
+++ clinin-backend/src/main/java/com/grauation/clinic/ClininBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grauation.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClininBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClininBackendApplication.class, args);
+	}
+
+}
---
+++ clinin-backend/src/test/java/com/grauation/clinic/ClininBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grauation.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClininBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
